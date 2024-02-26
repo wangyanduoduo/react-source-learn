1f2573c33e977b8094542150768ee7a6acaa00f3
wangyanduoduo <140472168+wangyanduoduo@users.noreply.github.com>
2024-02-26 11:47:53 +0800

    feat: init

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,8 +2,8 @@
  * @Author: wy
  * @Date: 2024-02-26 10:53:50
  * @LastEditors: wy
- * @LastEditTime: 2024-02-26 10:55:23
- * @FilePath: /笔记/react-source-learn/commitlintrc.js
+ * @LastEditTime: 2024-02-26 11:47:02
+ * @FilePath: /笔记/react-source-learn/.commitlintrc.js
  * @Description:
  */
 module.exports = {
