c1d6adca0b755317b237b74a22a8ce8d708a699d
wangyanduoduo <140472168+wangyanduoduo@users.noreply.github.com>
2024-02-26 11:24:49 +0800

    xx

---
+++ commitlintrc.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: wy
+ * @Date: 2024-02-26 10:53:50
+ * @LastEditors: wy
+ * @LastEditTime: 2024-02-26 10:55:23
+ * @FilePath: /笔记/react-source-learn/commitlintrc.js
+ * @Description:
+ */
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+};
--- packages/test.js
+++ packages/test.js
@@ -2,8 +2,8 @@
  * @Author: wy
  * @Date: 2024-02-23 18:00:42
  * @LastEditors: wy
- * @LastEditTime: 2024-02-23 18:00:47
+ * @LastEditTime: 2024-02-26 11:24:19
  * @FilePath: /笔记/react-source-learn/packages/test.js
  * @Description:
  */
-const a = 123;
+// const a = 123;
